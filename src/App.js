b382db7fce72f26709d9eff5331090e69bcd39c5
oscardavidrm <oscar@blockchainacademy.mx>
2019-11-09 10:51:05 -0600

    feature (App.js) Enable router to enroute app.
    Installed react-router-dom to support static routing on frontend.
    
    No breaking changes.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+import {Admin} from './views';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/admin" component={Admin} />
+    </Switch>
   );
-}
+};
 
 export default App;
